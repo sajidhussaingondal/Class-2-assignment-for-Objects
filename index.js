4b2d293230b453f8acac6bf98be52e104716cdef
sajidhussaingondal <sajidgondal@gmail.com>
2023-01-11 18:09:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,22 @@ this keyword
 4- Make 4 Methods Which Takes 2 Parameters And Do Addition,Subtraction,Multiplication And Division
 */
 
-const companyDetails = new Object(); //An empty Object (companyDetails) is created using new keyword. 
+let companyDetails = new Object(); //An empty Object (companyDetails) is created using new keyword. 
 companyDetails.companyName ="MAG Engineering";  //First property and it's value given.
 companyDetails.services ="Engineering design and construction services"; //Second property and it's value given.
 companyDetails.location = "110 DHA Lahore, Main Boulevard"; //Third property and it's value given.
 companyDetails.ManagingDirector = "SAJID HUSSAIN"; //Third property and it's value given.
 console.log(companyDetails); //Object (companyDetails) printed in console window of browser.
 
+
+companyDetails = {
+    companyName: "Sigma Marketing",
+    services: "Marketing",
+    location: "Islamabad",
+    ManagingDirector:"Rehan"
+}
+
+console.log(companyDetails);
 
 function myName (firstName, lastName){     //A function is created with two parameters in small braces.
     console.log(firstName + " " + lastName);    //Content of the function.
