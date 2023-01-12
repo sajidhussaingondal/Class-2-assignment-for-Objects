78391b60f4fac8b11368f44e6e47ae8176dd6808
sajidhussaingondal <sajidgondal@gmail.com>
2023-01-12 08:36:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,45 +8,48 @@ this keyword
 
 3- Make A Function Which Prints Your Name 
 
-4- Make 4 Methods Which Takes 2 Parameters And Do Addition,Subtraction,Multiplication And Division
 */
 
-let companyDetails = new Object(); //An empty Object (companyDetails) is created using new Object keyword. 
-companyDetails.companyName ="MAG Engineering";  //First property and it's value given.
-companyDetails.services ="Engineering design and construction services"; //Second property and it's value given.
+let companyDetails = new Object(); //An empty Object (companyDetails) is created using new Object keyword.
+companyDetails.companyName = "MAG Engineering"; //First property and it's value given.
+companyDetails.services = "Engineering design and construction services"; //Second property and it's value given.
 companyDetails.location = "110 DHA Lahore, Main Boulevard"; //Third property and it's value given.
 companyDetails.ManagingDirector = "SAJID HUSSAIN"; //Third property and it's value given.
-companyDetails.completedProjects = function(){
-let P1 ="1-Multistory residential bilding, covered area = 20340 square feet."; 
-return this.P1;
-let P2 ="2-Multistory commercial bilding, covered area = 10000 square feet."; 
-return this.P2;
-let P3 ="3-ten number of houses, each house covered area = 2500 square feet."; 
-return this.P3;
-}
+companyDetails.completedProjects = function () {
+  let P1 =
+    "1-Multistory residential bilding, covered area = 20340 square feet.";
+  return this.P1;
+};
 // completedProjects();
 console.log(companyDetails); //Object (companyDetails) printed in console window of browser.
 
-
 companyDetails = {
-    companyName: "Sigma Marketing",
-    services: "Marketing",
-    location: "Islamabad",
-    ManagingDirector:"Rehan"
-}
+  companyName: "Sigma Marketing",
+  services: "Marketing",
+  location: "Islamabad",
+  ManagingDirector: "Rehan",
+};
 
 console.log(companyDetails);
 
-function myName (firstName, lastName){     //A function is created with two parameters in small braces.
-    console.log(firstName + " " + lastName);    //Content of the function.
+function myName(firstName, lastName) {
+  //A function is created with two parameters in small braces.
+  console.log(firstName + " " + lastName); //Content of the function.
 }
-myName("Sajid", "Hussain");            // Function has been called by passing two arguments.
+myName("Sajid", "Hussain"); // Function has been called by passing two arguments.
 
-function doCalculations (a, b){  //A function is created with two parameters in small braces.
-    console.log(a+b);
-    console.log(a-b);
-    console.log(a*b);
-    console.log(a/b);
+// 4- Make 4 Methods Which Takes 2 Parameters And Do Addition,Subtraction,Multiplication And Division
 
+const objCalculationsMethods = {
+  doAddition: function () {
+    return n1 + n2;
+  },
+};
+function doCalculations(a, b) {
+  //A function is created with two parameters in small braces.
+  console.log(a + b);
+  console.log(a - b);
+  console.log(a * b);
+  console.log(a / b);
 }
 doCalculations(10, 2);
